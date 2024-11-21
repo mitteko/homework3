9937a1abb9bf5b6741ceea9bddbe1927ad7a2733
Ulyana Novoselova <uliana.a.novoselova@gmail.com>
2024-11-21 22:05:04 +0300

    last changes

--- File/Program.cs
+++ File/Program.cs
@@ -11,8 +11,9 @@ namespace File
             Task1();
             Task2();
             Task3();
-        
-        
+            Task4();
+            Task5();
+
         }
 
 
@@ -101,7 +102,105 @@ namespace File
         //3
         static void Task3()
         {
+            /// 
+
+            Console.WriteLine("3)");
+            Console.WriteLine("Выходные данные в соответствии с таблицей.");
+
+            Console.Write("Введите строку: ");
+            string input = Console.ReadLine().ToUpper();
+            switch (input)
+            {
+                case "JABRONI":
+                    Console.WriteLine("Patron Tequila");
+                    break;
+                case "SCHOOL COUNSELOR":
+                    Console.WriteLine("Anything with Alcohol");
+                    break;
+                case "PROGRAMMER":
+                    Console.WriteLine("Hipster Craft Beer");
+                    break;
+                case "BIKE GANG MEMBER":
+                    Console.WriteLine("Moonshine");
+                    break;
+                case "POLITICIAN":
+                    Console.WriteLine("your tax dollars");
+                    break;
+                case "RAPPER":
+                    Console.WriteLine("Cristal");
+                    break;
+                default:
+                    Console.WriteLine("Beer");
+                    break;
+            }
+        }
+
+
+        enum Week
+        {
+            Понедельник = 1,
+            Вторник = 2,
+            Среда = 3,
+            Четверг = 4,
+            Пятница = 5,
+            Суббота = 6,
+            Воскресенье = 7
+        }
+        //4
+        static void Task4()
+        {
+            /// Составить программу, которая в зависимости от порядкового номера дня недели (1, 2, ...,7) выводит на экран его название
+            /// (понедельник, вторник, ..., воскресенье). Использовать enum.
+
+            Console.WriteLine("4)");
+            Console.WriteLine("Порядковый номер дня-его название.");
+
+            byte num_day;
+            while (true)
+            {
+
+                Console.Write("Введите порядковый номер дня недели: ");
+                string day = Console.ReadLine();
+                if (byte.TryParse(day, out num_day))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Допустимы значения от 1 до 7");
+                }
+               
+            }
+
+            Console.WriteLine($"{num_day} день недели это {(Week)num_day}");
+
 
         }
+
+
+
+        //5
+        static void Task5()
+        {
+
+            /// Создать массив строк. При помощи foreach обойти весь массив. При встрече элемента "Hello Kitty" или "Barbie doll" 
+            /// необходимо положить их в “сумку”, т.е.прибавить к результату. Вывести на экран сколько кукол в “сумке”.
+
+            Console.WriteLine("5)");
+            Console.WriteLine("Вывести на экран сколько кукол в “сумке”.");
+
+            string[] strings = {"Hello Kitty", "Barbie doll", "Monster High", " Bratz", "Hello Kitty"};
+            int result = 0;
+            foreach (string s in strings)
+            {
+                if ( s == "Hello Kitty" || s == "Barbie doll" )
+                {
+                    result++;
+                }
+            }
+            Console.WriteLine($"Количество кукол в сумке - {result}");
+            Console.ReadKey();
+        }
+
     }
 }
